6926b935ad99f389d40b8dbcccbc4256ccab3cc8
Anjan Roy <anjanroy@yandex.com>
2021-11-23 03:21:27 +0000

    small refactoring, row is printed on screen using different function

--- Makefile
+++ Makefile
@@ -5,10 +5,13 @@ INCLUDES = -I./include
 PROG = run
 DFLAGS = -D$(shell echo $(or $(DEVICE),default) | tr a-z A-Z)
 
-$(PROG): main.o bench_ctbn.o
+$(PROG): main.o utils.o bench_ctbn.o
 	$(CXX) $(SYCLFLAGS) $^ -o $@
 
 main.o: main.cpp include/bench_ctbn.hpp include/types.hpp
+	$(CXX) $(CXXFLAGS) $(SYCLFLAGS) $(DFLAGS) -c $^ $(INCLUDES)
+
+utils.o: utils.cpp include/utils.hpp
 	$(CXX) $(CXXFLAGS) $(SYCLFLAGS) $(DFLAGS) -c $^ $(INCLUDES)
 
 bench_ctbn.o: bench_ctbn.cpp
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <bench_ctbn.hpp>
-#include <iomanip>
-#include <iostream>
 #include <types.hpp>
+#include <utils.hpp>
 
 constexpr uint64_t ITR_COUNT = 1ul << 10;
 constexpr uint64_t WG_SIZE = 1ul << 7;
@@ -18,11 +17,15 @@ int main(int argc, char **argv) {
 #endif
 
   sycl::queue q{d};
+  std::cout << "running on " << d.get_info<sycl::info::device::name>() << "\n"
+            << std::endl;
 
+  std::cout << "Addition on F(2^64 - 2^32 + 1)\n" << std::endl;
   std::cout << std::setw(11) << "dimension"
             << "\t\t" << std::setw(10) << "iterations"
             << "\t\t" << std::setw(15) << "total"
-            << "\t\t" << std::setw(20) << "avg" << std::endl;
+            << "\t\t" << std::setw(20) << "avg"
+            << "\t\t" << std::setw(24) << "ops/ sec" << std::endl;
 
   for (uint64_t i = 7; i <= 13; i++) {
     uint64_t dim = 1ul << i;
@@ -33,13 +36,50 @@ int main(int argc, char **argv) {
     int64_t tm =
         std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
             .count();
-    std::cout << std::setw(5) << std::left << dim << "x" << std::setw(5)
-              << std::right << dim << "\t\t" << std::setw(8) << std::right
-              << ITR_COUNT << "\t\t" << std::setw(15) << std::right << tm
-              << " ns"
-              << "\t\t" << std::setw(15) << std::right
-              << (double)tm / (double)(dim * dim * ITR_COUNT) << " ns"
-              << std::endl;
+    double tm_per_op = (double)tm / (double)(dim * dim * ITR_COUNT);
+    print_benchmark_table_row(dim, ITR_COUNT, tm, tm_per_op);
+  }
+
+  std::cout << "\nSubtraction on F(2^64 - 2^32 + 1)" << std::endl;
+  std::cout << "\n"
+            << std::setw(11) << "dimension"
+            << "\t\t" << std::setw(10) << "iterations"
+            << "\t\t" << std::setw(15) << "total"
+            << "\t\t" << std::setw(20) << "avg"
+            << "\t\t" << std::setw(24) << "ops/ sec" << std::endl;
+
+  for (uint64_t i = 7; i <= 13; i++) {
+    uint64_t dim = 1ul << i;
+    tp start = std::chrono::system_clock::now();
+    benchmark_ff_p_t_subtraction(q, dim, WG_SIZE, ITR_COUNT).wait();
+    tp end = std::chrono::system_clock::now();
+
+    int64_t tm =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+            .count();
+    double tm_per_op = (double)tm / (double)(dim * dim * ITR_COUNT);
+    print_benchmark_table_row(dim, ITR_COUNT, tm, tm_per_op);
+  }
+
+  std::cout << "\nMultiplication on F(2^64 - 2^32 + 1)" << std::endl;
+  std::cout << "\n"
+            << std::setw(11) << "dimension"
+            << "\t\t" << std::setw(10) << "iterations"
+            << "\t\t" << std::setw(15) << "total"
+            << "\t\t" << std::setw(20) << "avg"
+            << "\t\t" << std::setw(24) << "ops/ sec" << std::endl;
+
+  for (uint64_t i = 7; i <= 13; i++) {
+    uint64_t dim = 1ul << i;
+    tp start = std::chrono::system_clock::now();
+    benchmark_ff_p_t_multiplication(q, dim, WG_SIZE, ITR_COUNT).wait();
+    tp end = std::chrono::system_clock::now();
+
+    int64_t tm =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+            .count();
+    double tm_per_op = (double)tm / (double)(dim * dim * ITR_COUNT);
+    print_benchmark_table_row(dim, ITR_COUNT, tm, tm_per_op);
   }
 
   return 0;
