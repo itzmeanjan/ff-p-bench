cf5381c96c8506addc2b128d219828d22345d126
Anjan Roy <anjanroy@yandex.com>
2021-11-23 03:46:09 +0000

    more benchmarks on 64-bit prime field i.e. ops modular {division, inversion, exponentiation}

--- include/bench_ctbn.hpp
+++ include/bench_ctbn.hpp
@@ -3,7 +3,11 @@
 #include <ctbignum/ctbignum.hpp>
 
 using namespace cbn::literals;
-using ff_p_t = decltype(cbn::Zq(18446744069414584321_ZL));
+
+constexpr auto mod = 18446744069414584321_ZL;
+constexpr auto mod_bn = cbn::to_big_int(mod);
+
+using ff_p_t = decltype(cbn::Zq(mod));
 
 sycl::event benchmark_ff_p_t_addition(sycl::queue &q, const uint32_t dim,
                                       const uint32_t wg_size,
@@ -14,5 +18,17 @@ sycl::event benchmark_ff_p_t_subtraction(sycl::queue &q, const uint32_t dim,
                                          const uint32_t itr_count);
 
 sycl::event benchmark_ff_p_t_multiplication(sycl::queue &q, const uint32_t dim,
+                                            const uint32_t wg_size,
+                                            const uint32_t itr_count);
+
+sycl::event benchmark_ff_p_t_division(sycl::queue &q, const uint32_t dim,
+                                      const uint32_t wg_size,
+                                      const uint32_t itr_count);
+
+sycl::event benchmark_ff_p_t_inversion(sycl::queue &q, const uint32_t dim,
+                                       const uint32_t wg_size,
+                                       const uint32_t itr_count);
+
+sycl::event benchmark_ff_p_t_exponentiation(sycl::queue &q, const uint32_t dim,
                                             const uint32_t wg_size,
                                             const uint32_t itr_count);
