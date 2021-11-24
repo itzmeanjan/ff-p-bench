9b3a4e9bd1aa9b8927a221991a2b9e67ec28231a
Anjan Roy <anjanroy@yandex.com>
2021-11-24 11:46:26 +0000

    as corrected by @niekbouman, that compiler will probably optimize too much, if `tmp` variable in benchmark kernels doesn't have any computational significance, so I decided to store tmp at end of work-item's computation to local memory

--- bench_p256_ctbn.cpp
+++ bench_p256_ctbn.cpp
@@ -3,114 +3,192 @@
 sycl::event benchmark_ff_p256_t_addition(sycl::queue &q, const uint32_t dim,
                                          const uint32_t wg_size,
                                          const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Addition>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op1(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p256_t op2(
-            904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 + op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Addition>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op1(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+          ff_p256_t op2(
+              904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 + op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p256_t_subtraction(sycl::queue &q, const uint32_t dim,
                                             const uint32_t wg_size,
                                             const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Subtraction>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op1(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p256_t op2(
-            904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 - op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Subtraction>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op1(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+          ff_p256_t op2(
+              904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 - op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p256_t_multiplication(sycl::queue &q,
                                                const uint32_t dim,
                                                const uint32_t wg_size,
                                                const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Multiplication>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op1(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p256_t op2(
-            904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 * op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Multiplication>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op1(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+          ff_p256_t op2(
+              904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 * op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p256_t_division(sycl::queue &q, const uint32_t dim,
                                          const uint32_t wg_size,
                                          const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Division>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op1(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p256_t op2(
-            904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 / op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Division>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op1(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+          ff_p256_t op2(
+              904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 / op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p256_t_inversion(sycl::queue &q, const uint32_t dim,
                                           const uint32_t wg_size,
                                           const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Inversion>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += static_cast<ff_p256_t>(cbn::mod_inv(op.data, mod_p256_bn));
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Inversion>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += static_cast<ff_p256_t>(cbn::mod_inv(op.data, mod_p256_bn));
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p256_t_exponentiation(sycl::queue &q,
                                                const uint32_t dim,
                                                const uint32_t wg_size,
                                                const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p256_Exponentiation>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p256_t op1(
-            3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p256_t op2(
-            904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p256_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p256_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += static_cast<ff_p256_t>(
-              cbn::mod_exp(op1.data, op2.data, mod_p256_bn));
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p256_Exponentiation>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p256_t op1(
+              3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
+          ff_p256_t op2(
+              904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+
+          ff_p256_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += static_cast<ff_p256_t>(
+                cbn::mod_exp(op1.data, op2.data, mod_p256_bn));
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
--- bench_p64_ctbn.cpp
+++ bench_p64_ctbn.cpp
@@ -3,103 +3,181 @@
 sycl::event benchmark_ff_p64_t_addition(sycl::queue &q, const uint32_t dim,
                                         const uint32_t wg_size,
                                         const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Addition>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op1(2147483648_ZL);
-        ff_p64_t op2(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 + op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Addition>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op1(2147483648_ZL);
+          ff_p64_t op2(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 + op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p64_t_subtraction(sycl::queue &q, const uint32_t dim,
                                            const uint32_t wg_size,
                                            const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Subtraction>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op1(2147483648_ZL);
-        ff_p64_t op2(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 - op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Subtraction>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op1(2147483648_ZL);
+          ff_p64_t op2(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 - op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p64_t_multiplication(sycl::queue &q,
                                               const uint32_t dim,
                                               const uint32_t wg_size,
                                               const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Multiplication>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op1(2147483648_ZL);
-        ff_p64_t op2(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 * op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Multiplication>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op1(2147483648_ZL);
+          ff_p64_t op2(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 * op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p64_t_division(sycl::queue &q, const uint32_t dim,
                                         const uint32_t wg_size,
                                         const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Division>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op1(2147483648_ZL);
-        ff_p64_t op2(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += (op1 / op2);
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Division>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op1(2147483648_ZL);
+          ff_p64_t op2(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += (op1 / op2);
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p64_t_inversion(sycl::queue &q, const uint32_t dim,
                                          const uint32_t wg_size,
                                          const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Inversion>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += static_cast<ff_p64_t>(cbn::mod_inv(op.data, mod_p64_bn));
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Inversion>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += static_cast<ff_p64_t>(cbn::mod_inv(op.data, mod_p64_bn));
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
 
 sycl::event benchmark_ff_p64_t_exponentiation(sycl::queue &q,
                                               const uint32_t dim,
                                               const uint32_t wg_size,
                                               const uint32_t itr_count) {
-  sycl::event evt = q.parallel_for<class kernelFF_p64_Exponentiation>(
-      sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
-      [=](sycl::nd_item<2> it) {
-        ff_p64_t op1(2147483648_ZL);
-        ff_p64_t op2(576460752303423488_ZL);
+  return q.submit([&](sycl::handler &h) {
+    // allocate some space in local memory where I'll write
+    // some garbage data, at end of computation so that compiler
+    // doesn't end up optimizing too much !
+    sycl::accessor<ff_p64_t, 1, sycl::access_mode::read_write,
+                   sycl::target::local>
+        lds{sycl::range<1>{wg_size}, h};
 
-        ff_p64_t tmp(0_ZL);
-        for (uint64_t i = 0ul; i < itr_count; i++) {
-          tmp += static_cast<ff_p64_t>(
-              cbn::mod_exp(op1.data, op2.data, mod_p64_bn));
-        }
-      });
-  return evt;
+    h.parallel_for<class kernelFF_p64_Exponentiation>(
+        sycl::nd_range<2>{sycl::range<2>{dim, dim}, sycl::range<2>{1, wg_size}},
+        [=](sycl::nd_item<2> it) {
+          const size_t loc_lid = it.get_local_linear_id();
+
+          ff_p64_t op1(2147483648_ZL);
+          ff_p64_t op2(576460752303423488_ZL);
+
+          ff_p64_t tmp(0_ZL);
+          for (uint64_t i = 0ul; i < itr_count; i++) {
+            tmp += static_cast<ff_p64_t>(
+                cbn::mod_exp(op1.data, op2.data, mod_p64_bn));
+          }
+
+          // every work item writes back to local memory
+          lds[loc_lid] = tmp;
+        });
+  });
 }
