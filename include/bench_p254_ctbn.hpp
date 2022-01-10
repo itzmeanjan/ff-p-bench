25323050af288d23d4e59283289948ae661d4dc6
Anjan Roy <anjanroy@yandex.com>
2022-01-10 15:37:05 +0000

    removed unused local memory allocation from 254-bit prime field benchmark kernels

--- include/bench_p254_ctbn.hpp
+++ include/bench_p254_ctbn.hpp
@@ -18,13 +18,6 @@ benchmark_ff_p254_t_addition(sycl::queue& q,
                              ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Addition>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -57,13 +50,6 @@ benchmark_ff_p254_t_subtraction(sycl::queue& q,
                                 ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Subtraction>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -96,13 +82,6 @@ benchmark_ff_p254_t_multiplication(sycl::queue& q,
                                    ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Multiplication>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -135,13 +114,6 @@ benchmark_ff_p254_t_division(sycl::queue& q,
                              ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Division>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -174,13 +146,6 @@ benchmark_ff_p254_t_inversion(sycl::queue& q,
                               ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Inversion>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -211,13 +176,6 @@ benchmark_ff_p254_t_exponentiation(sycl::queue& q,
                                    ff_p254_t* const mem)
 {
   return q.submit([&](sycl::handler& h) {
-    // allocate some space in local memory where I'll write
-    // some garbage data, at end of computation so that compiler
-    // doesn't end up optimizing too much !
-    sycl::
-      accessor<ff_p254_t, 1, sycl::access_mode::read_write, sycl::target::local>
-        lds{ sycl::range<1>{ wg_size }, h };
-
     h.parallel_for<class kernelFF_p254_Exponentiation>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -226,8 +184,7 @@ benchmark_ff_p254_t_exponentiation(sycl::queue& q,
 
         ff_p254_t op(
           3618502788666131106986593281521497120414687020801267626233049500247285301247_ZL);
-        ff_p254_t tmp(
-          904625697166532776746648320380374280103671755200316906558262375061821325304_ZL);
+        ff_p254_t tmp(70368744177664_ZL);
 
         for (uint64_t i = 0ul; i < itr_count; i++) {
           tmp = static_cast<ff_p254_t>(
